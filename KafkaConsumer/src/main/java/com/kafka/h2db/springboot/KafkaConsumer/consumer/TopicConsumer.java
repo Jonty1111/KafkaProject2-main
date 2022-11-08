1e5032c48b685d136881eed6a3e0aa47ce502f2b
Jonty1111 <Jonty.Singla@jda.com>
2022-11-08 18:03:58 +0530

    g

--- KafkaConsumer/src/main/java/com/kafka/h2db/springboot/KafkaConsumer/consumer/TopicConsumer.java
+++ KafkaConsumer/src/main/java/com/kafka/h2db/springboot/KafkaConsumer/consumer/TopicConsumer.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
+//
 @Slf4j //setter//getter/constructor/table
 @RequiredArgsConstructor
 @Service
